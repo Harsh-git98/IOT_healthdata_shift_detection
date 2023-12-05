b67f824db4248d40c1748804a582f434ad0c5fa7
Harsh-git98 <128585057+Harsh-git98@users.noreply.github.com>
2023-12-05 23:37:13 +0530

    csvinclude butsigmatprob P-EWMA.cpp
    this code has majr problem with sigmat bcoz of increasing value of s2*s2. and how to judge the flag that its differnet from dataset

--- P-EWMA.cpp
+++ P-EWMA.cpp
@@ -1,66 +1,114 @@
 #include<iostream>
 #include<vector>
 #include <cmath>
+#include <fstream>
+#include <sstream>
 
 using namespace std;
-# define BETA 0.5
-# define lamda 0.9
+# define BETA 0.05
+# define lamda 0.95
+
+struct CSVRow {
+    int column1;
+    int column2;
+    int column3;
+};
+// 55 90 170 
+
+CSVRow parseCSV(const string& line) {
+    CSVRow row;
+    stringstream ss(line);
+    char comma; // to read the comma
+    ss >> row.column1 >> comma >> row.column2 >> comma >> row.column3;
+    return row;
+}
+
+
 double KT(double xt, double xtmean,double sigmat)
 {
     double kt=xt-xtmean;
     kt=kt/sigmat;
     return kt;
+    
 
 }
 double PT(int kt)
 {
-    double n= -(kt*kt)/2;
-    double ans=exp(n)/(sqrt(2)*M_1_PI);
+    double n= (-(kt*kt)/2);
+    double ans=exp(n)/(sqrt(2*M_1_PI));
+    
     return ans;
+    
 
 }
 
 int main()
 {
-    int time[200];
-    int heartbeat[200];
+   string filename = "data.csv";
+
+    ifstream file(filename);
+
+    
+    if (!file.is_open()) {
+        cerr << "Error opening file: " << filename << endl;
+        return 1; 
+    }
+
+    double time[200]={0};
+    double heartbeat[200]={0};
+    double num[200]={0};
+
+    string line;
+    int p=0;
+    while (getline(file, line)) {
+        
+        CSVRow row = parseCSV(line);
+        num[p]=row.column1;
+        time[p]=row.column2;
+        heartbeat[p]=row.column3;
+        p++;
+        
+    }
+
+    
+    file.close();
+
     double PEWMA[200];
     double xtmean=heartbeat[0];
-    double sigmat=1; 
+    double sigmat=0; 
     int settime =time[50];
-    int s1=heartbeat[0];
-    int s2=heartbeat[0];
-    for(int t=0;t<time[200];t++)
+    double s1=heartbeat[0];
+    double s2=heartbeat[0];
+    for(int t=0;t<200;t++)
     {
         double kt=KT(heartbeat[t],xtmean,sigmat);
         double pt=PT(kt);
-        PEWMA[t]=pt;
+        
         double lamdat=0;
         if(t<=settime)
         {
-            lamdat=1-1/time[t];
+            lamdat=1-(1/time[t]);
         }
         else{
             lamdat=(1- BETA*pt)*lamda;
-            if(heartbeat[t]>PEWMA[t-1])
+            if(abs(heartbeat[t]-PEWMA[t-1])>10)
             {
                 cout<<"anamoly detected at time:"<<time[t]<<endl;
             }
+            
         }
-
+        
         double s1=lamdat*heartbeat[t] + (1-lamdat)*s1;
-        double s2=lamdat*heartbeat[t] + (1-lamdat)*s2*s2;
-
+        double s2=(lamdat*heartbeat[t] + (1-lamdat)*s2*s2);
         xtmean=s1;
-        sigmat=sqrt((s2)-(s1*s1));
-
+        PEWMA[t] = s1;
+        sigmat=sqrt(abs(s2-(s1*s1)));
+        
     }
 
-    cout<<endl<<endl<<endl<<endl;
-    for(int i=0;i<time[200];i++)
+    for(int i=0;i<200;i++)
     {
         cout<<time[i]<<" "<<heartbeat[i]<<" "<<PEWMA[i]<<endl;
-
     }
 
 
